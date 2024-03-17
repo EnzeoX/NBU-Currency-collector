acc2c4659247465974375fc691601870a78bcb9e
Boyko Nikolay <enzeox@gmail.com>
2024-03-17 18:46:18 +0200

    Added date formatting as ISO 8601

--- src/main/java/org/nbu/dto/NbuDto.java
+++ src/main/java/org/nbu/dto/NbuDto.java
@@ -1,6 +1,11 @@
 package org.nbu.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -34,6 +39,9 @@ public class NbuDto {
     private LocalDate exchangeDate;
 
     @JsonProperty("date_add")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime dateAdd;
 
 }
