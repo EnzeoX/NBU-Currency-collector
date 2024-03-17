a9aba184733835a1d4b77c5952814fc94d1df966
Boyko Nikolay <enzeox@gmail.com>
2024-03-17 18:42:38 +0200

    Simplified controller and delegated processing to handler Added simple logging

--- src/main/java/org/nbu/controller/CurrencyController.java
+++ src/main/java/org/nbu/controller/CurrencyController.java
@@ -1,5 +1,6 @@
 package org.nbu.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.nbu.dto.NbuDto;
@@ -23,19 +24,22 @@ public class CurrencyController {
     private final CurrencyRequestHandler requestHandler;
 
     @GetMapping("/get")
-    public ResponseEntity<List<NbuDto>> getAllCurrency() {
+    public ResponseEntity<List<NbuDto>> getActualCurrency(HttpServletRequest request) {
+        log.info("Request to get actual currency info by date. Request from {}", request.getRemoteAddr());
         return requestHandler.getAllCurrency();
     }
 
     @GetMapping("/get/by-date/{date}")
-    public ResponseEntity<?> getAllCurrencyByDate(@PathVariable("date") String date) {
-        List<NbuDto> collectedData = requestHandler.getAllCurrencyByDate(date);
-        return ResponseEntity.status(HttpStatus.OK).body(collectedData);
+    public ResponseEntity<?> getAllCurrencyByDate(@PathVariable("date") String date,
+                                                  HttpServletRequest request) {
+        log.info("Request to get currency info by date. Request from {}", request.getRemoteAddr());
+        return requestHandler.getAllCurrencyByDate(date);
     }
 
     @PostMapping("/delete/by-date/{date}")
-    public ResponseEntity<?> deleteCurrencyByDate(@PathVariable("date") String date) {
-        requestHandler.deleteCurrencyByDate(date);
-        return ResponseEntity.status(HttpStatus.OK).body("Deletion by date" + date + " ok"); //TODO get better response after deletion
+    public ResponseEntity<?> deleteCurrencyByDate(@PathVariable("date") String date,
+                                                  HttpServletRequest request) {
+        log.info("Request to delete currency info by date. Request from {}", request.getRemoteAddr());
+        return requestHandler.deleteCurrencyByDate(date);
     }
 }
