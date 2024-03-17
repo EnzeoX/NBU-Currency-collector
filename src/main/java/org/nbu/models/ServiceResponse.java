de0fe46feb4aba7c1c3b429e35e194ba3db82ef1
Boyko Nikolay <enzeox@gmail.com>
2024-03-17 18:48:55 +0200

    Added service response model

---
+++ src/main/java/org/nbu/models/ServiceResponse.java
@@ -0,0 +1,34 @@
+package org.nbu.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author Nikolay Boyko
+ */
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ServiceResponse {
+
+    @JsonProperty("message")
+    private String message;
+
+    @JsonProperty("status")
+    private String status;
+
+    @JsonProperty("time")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime time = LocalDateTime.now();
+}
