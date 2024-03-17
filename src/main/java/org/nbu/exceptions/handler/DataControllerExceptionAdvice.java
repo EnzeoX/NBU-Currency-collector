679c0587e8eb013020f5032e6f435a8aa2456f66
Boyko Nikolay <enzeox@gmail.com>
2024-03-17 18:44:14 +0200

    Simple exception handling with response message

--- src/main/java/org/nbu/exceptions/handler/DataControllerExceptionAdvice.java
+++ src/main/java/org/nbu/exceptions/handler/DataControllerExceptionAdvice.java
@@ -1,6 +1,8 @@
 package org.nbu.exceptions.handler;
 
 import lombok.extern.slf4j.Slf4j;
+import org.nbu.models.ServiceResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,7 +17,16 @@ public class DataControllerExceptionAdvice {
 
     @ExceptionHandler({Exception.class})
     public ResponseEntity<?> handleException(Exception e) {
+        String errorName = e.getClass().getSimpleName();
         log.error("Error occurred, catching in Controller Advice, message: {}", e.getMessage());
-        return null;
+        ServiceResponse serviceResponse = new ServiceResponse();
+        serviceResponse.setStatus("ERROR");
+        serviceResponse.setMessage(e.getMessage());
+        switch (errorName) {
+            case "NullPointerException":
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(serviceResponse);
+            default:
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(serviceResponse);
+        }
     }
 }
