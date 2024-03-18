194bd2ee1f170af9e1e684030c94fcd25e783fa5
Boyko Nikolay <enzeox@gmail.com>
2024-03-18 18:00:51 +0200

    Added tests for each endpoint in for CurrencyController

--- src/test/java/org/nbu/nbucurrencycollector/controller/CurrencyControllerTest.java
+++ src/test/java/org/nbu/nbucurrencycollector/controller/CurrencyControllerTest.java
@@ -13,7 +13,7 @@ import org.nbu.dto.CurrencyDto;
 import org.nbu.handler.CurrencyRequestHandler;
 import org.nbu.models.NbuDataModel;
 import org.nbu.models.ServiceResponse;
-import org.nbu.service.ICurrencyService;
+import org.nbu.service.CurrencyService;
 import org.nbu.utils.DataMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -21,10 +21,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import javax.xml.transform.OutputKeys;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -33,8 +31,9 @@ import java.util.List;
 import java.util.Map;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * @author Nikolay Boyko
@@ -53,9 +52,6 @@ public class CurrencyControllerTest {
 
     @MockBean
     private CurrencyRequestHandler requestHandler;
-
-    @MockBean
-    private ICurrencyService<List<CurrencyDto>, LocalDate> currencyService;
 
     private final Map<LocalDate, List<NbuDataModel>> listOfData = new HashMap<>();
 
@@ -86,12 +82,11 @@ public class CurrencyControllerTest {
         LocalDate actual = LocalDate.of(2024, 3, 18);
         LocalDateTime now = LocalDateTime.now();
         actualData.addAll(
-                listOfData.get(actual).stream()
+                listOfData.get(actual)
+                        .stream()
                         .map(model -> DataMapper.nbuModelToDto(model, now))
                         .toList()
         );
-        actualDataStringJson = mapper.writeValueAsString(this.actualData);
-
         dataByDate.addAll(
                 listOfData.get(LocalDate.of(2024, 1, 1))
                         .stream()
@@ -99,6 +94,7 @@ public class CurrencyControllerTest {
                         .toList()
         );
         dataByDateStringJson = mapper.writeValueAsString(this.dataByDate);
+        actualDataStringJson = mapper.writeValueAsString(this.actualData);
     }
 
     @Test
@@ -138,22 +134,53 @@ public class CurrencyControllerTest {
     @Test
     @DisplayName("Test method \"getAllCurrencyByDate\" - response ok with data inside body")
     public void getCurrencyByDateWithDataOk() throws Exception {
-        ResponseEntity<List<CurrencyDto>> response = ResponseEntity.status(HttpStatus.OK).body(dataByDate);
-        Mockito.when(this.requestHandler.getAllCurrency()).thenReturn(response);
-        mvc.perform(get("/api/v1/currency/get/by-date/1995-01-01"))
+        ResponseEntity<List<CurrencyDto>> response = ResponseEntity.status(HttpStatus.OK).body(this.dataByDate);
+        Mockito.when(this.requestHandler.getAllCurrencyByDate(LocalDate.of(2024, 1, 1))).thenReturn(response);
+        mvc.perform(get("/api/v1/currency/get/by-date/2024-01-01"))
                 .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.content().json(dataByDateStringJson));
+                .andExpect(MockMvcResultMatchers.content().json(this.dataByDateStringJson));
     }
 
     @Test
     @DisplayName("Test method \"getAllCurrencyByDate\" - expecting exception because of incorrect date")
     public void getCurrencyByDateIncorrectDateProvided() throws Exception {
+        mvc.perform(get("/api/v1/currency/get/by-date/112312323"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message", CoreMatchers.is("Can't parse provided URL parameter")));
+    }
+
+    @Test
+    @DisplayName("Test method \"deleteCurrencyByDate\" - expecting OK status response")
+    public void deleteCurrencyByDateCorrectExpectOkStatus() throws Exception {
+        LocalDate date = LocalDate.of(2024, 1, 1);
         ServiceResponse serviceResponse = new ServiceResponse();
-        serviceResponse.setMessage("Can't parse provided URL parameter");
-        serviceResponse.setStatus("ERROR");
-        ResponseEntity<ServiceResponse> response = ResponseEntity.status(HttpStatus.BAD_REQUEST).body(serviceResponse);
-        ResultActions actions = mvc.perform(get("/api/v1/currency/get/by-date/112312323"));
-        actions
+        serviceResponse.setStatus("OK");
+        serviceResponse.setMessage(String.format("Data deletion by provided date %s successful", date));
+        ResponseEntity<ServiceResponse> response = ResponseEntity.status(HttpStatus.OK).body(serviceResponse);
+        Mockito.when(this.requestHandler.deleteCurrencyByDate(date)).thenReturn(response);
+        mvc.perform(post("/api/v1/currency/delete/by-date/2024-01-01"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status", CoreMatchers.is("OK")));
+    }
+
+    @Test
+    @DisplayName("Test method \"deleteCurrencyByDate\" - expecting successful message")
+    public void deleteCurrencyByDateCorrectExpectSuccessfulMessage() throws Exception {
+        LocalDate date = LocalDate.of(2024, 1, 1);
+        ServiceResponse serviceResponse = new ServiceResponse();
+        serviceResponse.setStatus("OK");
+        serviceResponse.setMessage(String.format("Data deletion by provided date %s successful", date));
+        ResponseEntity<ServiceResponse> response = ResponseEntity.status(HttpStatus.OK).body(serviceResponse);
+        Mockito.when(this.requestHandler.deleteCurrencyByDate(date)).thenReturn(response);
+        mvc.perform(post("/api/v1/currency/delete/by-date/2024-01-01"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message", CoreMatchers.is("Data deletion by provided date 2024-01-01 successful")));
+    }
+
+    @Test
+    @DisplayName("Test method \"deleteCurrencyByDate\" - expecting exception because of incorrect date")
+    public void deleteCurrencyByDateCorrectExceptionExpected() throws Exception {
+        mvc.perform(get("/api/v1/currency/get/by-date/112312323"))
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.message", CoreMatchers.is("Can't parse provided URL parameter")));
     }
