498c83c29ab4dc7ca032878a30b48a3a7aca649a
Boyko Nikolay <enzeox@gmail.com>
2024-03-18 17:59:57 +0200

    Changed profile condition Changed CurrencyService interface name and types

--- src/main/java/org/nbu/config/AppConfig.java
+++ src/main/java/org/nbu/config/AppConfig.java
@@ -1,9 +1,8 @@
 package org.nbu.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.nbu.dto.CurrencyDto;
 import org.nbu.repository.NbuDataRepository;
-import org.nbu.service.ICurrencyService;
+import org.nbu.service.CurrencyService;
 import org.nbu.service.impl.MockCurrencyService;
 import org.nbu.service.impl.NbuService;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -13,7 +12,6 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
 import java.time.LocalDate;
-import java.util.List;
 
 /**
  * @author Nikolay Boyko
@@ -24,16 +22,16 @@ public class AppConfig {
 
     @Bean
     @Primary
-    @Profile("dev")
+    @Profile("!mock")
     @Qualifier("nbuCurrencyService")
-    public ICurrencyService<List<CurrencyDto>, LocalDate> nbuCurrencyService(NbuDataRepository repository, ObjectMapper mapper) {
+    public CurrencyService<LocalDate> nbuCurrencyService(NbuDataRepository repository, ObjectMapper mapper) {
         return new NbuService(mapper, repository);
     }
 
     @Bean
     @Profile("mock")
     @Qualifier("mockedCurrencyService")
-    public ICurrencyService<List<CurrencyDto>, LocalDate> mockedCurrencyService() {
+    public CurrencyService<LocalDate> mockedCurrencyService() {
         return new MockCurrencyService();
     }
 }
